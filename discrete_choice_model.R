825b8b22351d5f90dbfe0d554b5877bcd8c3e28e
gonzalofichero <gonzalo.fce@gmail.com>
2021-11-25 16:35:41 +0100

    init code for discrete choice model Paper 1

---
+++ discrete_choice_model.R
@@ -0,0 +1,55 @@
+# Loading neccessary libraries 
+library(tidyverse)
+
+
+
+# Loading data:
+
+  # Inflow to BCN
+  inflow <- read_delim("C:/Users/ggarcia/Desktop/PhD GG/02 - Dissertation/101_Inflow_BCN/inflows_BCN_2011_2018.txt", 
+                       delim = "\t", col_names = TRUE)
+  # Grouping Barris with Toni´s definition (for lower count cases)
+  grup_barris_table <- read_delim("C:/Users/ggarcia/Desktop/PhD GG/02 - Dissertation/101_Inflow_BCN/tabla_conversion_agrupado_barrios.txt", 
+                                  delim = "\t", col_names = TRUE)
+  
+# Checking
+glimpse(inflow)
+glimpse(grup_barris_table)
+  
+  
+# Grouping by into Europeans vs Latinos vs East Europeans:
+inflow %>% 
+    mutate(nation = case_when(as.character(NOM_Naix) %in% c("Resta Unió Europea", "Itàlia", "França", "Regne Unir", "Alemanya") ~ "European",
+                              as.character(NOM_Naix) %in% c("Geòrgia", "Resta Europa", "Romania", "Rússia", "Ucraïna") ~ "European.East",
+                              as.character(NOM_Naix) %in% c("Argentina", "Veneçuela", "Colòmbia", "Brasil", "Mèxic", "Xile", "Perú", "Equador", "República Dominicana", "Hondures", "Uruguai", "Bolívia", "Paraguai", "Cuba", "Resta América") ~ "Latino",
+                              TRUE ~ as.character(NOM_Naix))) -> inflow
+
+
+# Adding Grouped Barris from Toni´s definition to compare flows:
+inflow %>% 
+  rename(BARRI_COD = BARRI_dest) %>% 
+  mutate(BARRI_COD = as.factor(str_pad(BARRI_COD, width=2, side="left", pad="0"))) %>% 
+  left_join(grup_barris_table %>% select(BARRI_COD,BARRI_AGRUP_TONI), by = "BARRI_COD") -> inflow2
+
+
+# Need to filter for Latinos + Europeans, 24 to 40 yos, University degree or higher, 
+# for 2018
+table(inflow2$Nivell_educ)
+table(inflow2$Edat_q)
+
+
+
+inflow2 %>% 
+  filter(Nivell_educ == "Estudis universitaris / CFGS grau superior") %>% 
+  filter(nation %in% c("European","Latino")) %>% 
+  filter(Edat_q %in% c("25 - 29", "30 - 34", "35 - 39")) %>% 
+  filter(Any %in% c(2016,2017,2018)) %>% 
+  group_by(Any, nation) %>% 
+  summarise(total = sum(Casos, na.rm=TRUE))
+
+-> inflow_foreign
+
+
+
+
+  
