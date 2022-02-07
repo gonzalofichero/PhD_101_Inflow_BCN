274e14c254a76df9a3a671958096dc982b9f3f4f
Gonzalo <gonzalo.fce@gmail.com>
2022-02-07 11:08:00 +0100

    add correlation plot for PCA in control variables add correlation plot for PCA in both control + amenities

--- correlation_features_bcn_full.R
+++ correlation_features_bcn_full.R
@@ -18,3 +18,42 @@ p.mat.barri73 <- cor.mtest(article1_correlations73)
 
 corrplot(cor(as.matrix(article1_correlations73)), 
          type="upper", p.mat = p.mat.barri73, sig.level = 0.01, insig = "blank")
+
+
+
+
+
+######################################################
+# Correlations when using PCA for control variables
+
+# Selecting the features that I'm using in full model
+indiv_barri73_fixed %>% 
+  select(BARRI_COD, avg_rent_2015, cultural_pop, bars,
+         Time_bike_Barceloneta) %>% 
+  unique() %>% 
+  left_join(control_vars_pca, by = "BARRI_COD") %>% 
+  left_join(amenities_pca, by = "BARRI_COD") -> article1_correlations_pca
+
+
+##### Correlation 1to1 for Full variables + PCAs #####
+p.mat.pca <- cor.mtest(article1_correlations_pca %>% select(avg_rent_2015, cultural_pop, bars,
+                                                            Time_bike_Barceloneta, control_pca_factor1,
+                                                            control_pca_factor2,
+                                                            control_pca_factor3))
+
+corrplot(cor(as.matrix(article1_correlations_pca %>% select(avg_rent_2015, cultural_pop, bars,
+                                                            Time_bike_Barceloneta, control_pca_factor1,
+                                                            control_pca_factor2,
+                                                            control_pca_factor3))), 
+         type="upper", p.mat = p.mat.pca, sig.level = 0.01, insig = "blank")
+
+
+##### Correlation 1to1 for Full PCAs #####
+p.mat.pca2 <- cor.mtest(article1_correlations_pca %>% select(avg_rent_2015, amenities_pc, control_pca_factor1,
+                                                            control_pca_factor2,
+                                                            control_pca_factor3))
+
+corrplot(cor(as.matrix(article1_correlations_pca %>% select(avg_rent_2015, amenities_pc, control_pca_factor1,
+                                                            control_pca_factor2,
+                                                            control_pca_factor3))), 
+         type="upper", p.mat = p.mat.pca2, sig.level = 0.01, insig = "blank")
