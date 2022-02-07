b910866ecb75461a39c3a0f5b05d68efaa05c2bd
Gonzalo <gonzalo.fce@gmail.com>
2022-02-07 10:52:54 +0100

    add PCA for control variables

--- pca_amenities.R
+++ pca_amenities.R
@@ -73,3 +73,56 @@ amenities_pca <- data.frame(cbind(data_barri2$BARRI_COD,pc$x[,1])) %>%
 
 
 
+
+###############################
+# PCA for control variables
+
+feature_control <- indiv_barri73_fixed %>% 
+                    select(sum_old, age_building, perc_left,
+                           perc_domi_uni_25_40, excess_uni, airbnbs) %>% 
+                    unique()
+
+
+# Checking correlation
+library(psych)
+
+pairs.panels(feature_control,
+             gap = 0,
+             pch=21)
+
+
+pc_control <- prcomp(feature_control,
+                     center = TRUE,
+                     scale. = TRUE)
+
+attributes(pc_control)
+
+# Check factors
+summary(pc_control)
+# First 3 factors gets 90% of total variance...
+# I'm reducing to half the variables
+
+
+# Trying to understand what the factors are...
+library(ggbiplot)
+ggbiplot(pc_control,
+         obs.scale = 1,
+         var.scale = 1,
+         ellipse = TRUE,
+         circle = TRUE,
+         ellipse.prob = 0.68) + 
+  scale_color_discrete(name = '') +
+  theme(legend.direction = 'horizontal',
+        legend.position = 'top')
+
+
+# Extract first factor
+amenities_pca <- data.frame(cbind(data_barri2$BARRI_COD,pc_control$x[,1:3])) %>% 
+  rename(BARRI_COD = X1,
+         control_pca_factor1 = X2,
+         control_pca_factor2 = X3,
+         control_pca_factor3 = X4) %>% 
+  mutate(control_pca_factor1 = as.double(control_pca_factor1),
+         control_pca_factor2 = as.double(control_pca_factor2),
+         control_pca_factor3 = as.double(control_pca_factor3))
+
