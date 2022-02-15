8c30eb945ca4c7b5d8aee454a26b2b6fe7ee3905
Gonzalo <gonzalo.fce@gmail.com>
2022-02-15 18:21:20 +0100

    add create transitory feature with PCA

--- pca_amenities.R
+++ pca_amenities.R
@@ -74,13 +74,59 @@ amenities_pca <- data.frame(cbind(data_barri2$BARRI_COD,pc$x[,1])) %>%
 
 
 
+###################################
+# PCA on transitority
+
+feature_transitority <- indiv_barri73_fixed %>% 
+                        select(sum_old, airbnbs) %>% 
+                        unique()
+
+
+# Checking correlation
+library(psych)
+
+pairs.panels(feature_transitority,
+             gap = 0,
+             pch=21)
+
+
+pc_transitority <- prcomp(feature_transitority,
+                     center = TRUE,
+                     scale. = TRUE)
+
+# Check factors
+summary(pc_transitority)
+# First 3 factors gets 90% of total variance...
+# I'm reducing to half the variables
+
+
+# Trying to understand what the factors are...
+library(ggbiplot)
+ggbiplot(pc_transitority,
+         obs.scale = 1,
+         var.scale = 1,
+         ellipse = TRUE,
+         circle = TRUE,
+         ellipse.prob = 0.68) + 
+  scale_color_discrete(name = '') +
+  theme(legend.direction = 'horizontal',
+        legend.position = 'top')
+
+
+# Extract first factor
+transitority_vars_pca <- data.frame(cbind(data_barri2$BARRI_COD,pc_transitority$x[,1])) %>% 
+                                    dplyr::rename(BARRI_COD = X1,
+                                                   control_pca_factor1 = X2) %>% 
+                                    mutate(control_pca_factor1 = as.double(control_pca_factor1))
+
+
 ###############################
 # PCA for control variables
 
 feature_control <- indiv_barri73_fixed %>% 
-                    select(sum_old, age_building, perc_left,
-                           perc_domi_uni_25_40, excess_uni, airbnbs) %>% 
-                    unique()
+  select(sum_old, age_building, perc_left,
+         perc_domi_uni_25_40, excess_uni, airbnbs) %>% 
+  unique()
 
 
 # Checking correlation
@@ -118,11 +164,10 @@ ggbiplot(pc_control,
 
 # Extract first factor
 control_vars_pca <- data.frame(cbind(data_barri2$BARRI_COD,pc_control$x[,1:3])) %>% 
-                    dplyr::rename(BARRI_COD = V1,
-                           control_pca_factor1 = PC1,
-                           control_pca_factor2 = PC2,
-                           control_pca_factor3 = PC3) %>% 
-                    mutate(control_pca_factor1 = as.double(control_pca_factor1),
-                           control_pca_factor2 = as.double(control_pca_factor2),
-                           control_pca_factor3 = as.double(control_pca_factor3))
-
+  dplyr::rename(BARRI_COD = V1,
+                control_pca_factor1 = PC1,
+                control_pca_factor2 = PC2,
+                control_pca_factor3 = PC3) %>% 
+  mutate(control_pca_factor1 = as.double(control_pca_factor1),
+         control_pca_factor2 = as.double(control_pca_factor2),
+         control_pca_factor3 = as.double(control_pca_factor3))
